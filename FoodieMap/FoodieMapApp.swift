bc4df73394616783b3f1b15a07f0d151c6d0694c
SaishTiwari <tiwarisaish381@gmail.com>
2024-07-30 20:01:44 +0545

    hi

---
+++ FoodieMap/FoodieMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodieMapApp.swift
+//  FoodieMap
+//
+//  Created by Saish Tiwari on 30/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct FoodieMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
